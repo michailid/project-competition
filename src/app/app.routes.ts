3b97cc5090d6c597e301f65f3a1e1e66a6870c70
michailid <michailidoumaria224@gmail.com>
2026-08-27 23:34:44 +0300

    add UI for home & register pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Register } from './pages/register/register';
+import { Events } from './pages/events/events';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: 'register',
+    component: Register,
+  },
+  {
+    path: 'events',
+    component: Events,
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+];
