79a6db0109c0a2bfcb61bf3c55be7fbd46991c1c
michailid <michailidoumaria224@gmail.com>
2026-08-29 15:51:03 +0300

    login form UI & register functionality

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  isLoginFormVisible = signal<boolean>(false);
+  http = inject(HttpClient);
+
+  registerObj: any = {
+    fullName: '',
+    email: '',
+    password: '',
+    collegeName: '',
+    role: '',
+  };
+
+  toggleForm() {
+    this.isLoginFormVisible.set(!this.isLoginFormVisible());
+  }
+
+  onRegister() {
+    this.http
+      .post('https://api.freeprojectapi.com/api/ProjectCompetition/register', this.registerObj)
+      .subscribe({
+        next: (res: any) => {
+          alert('Successfully registered');
+        },
+        error: (error) => {
+          alert(error.error);
+        },
+      });
+  }
+}
